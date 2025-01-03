8f8a0f19a3e24e1bd8dcafaa0d04be84765d97ee
Ishini <ishinishavindhya@gmail.com>
2025-01-03 11:20:27 +0530

    db connection

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,35 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const mysql = require('mysql2');
 
 app.use(express.json());
 app.use(cors());//resource sharing
 
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:"abcd123",
+    database: 'sys'
+})
+db.connect((err) => {
+    if(!err) {
+        console.log("Connected to database successfully");
+    }else{
+        console.log("Couldn't connect to database");
+    }
+})
+
 app.post('/new-task', (req, res) => {
     console.log(req.body);
+    const q = 'insert into todos (task, createdAt) values (?,?)';
+    db.query(q,[req.body.task,new Date()],(err,result) => {
+        if(err){
+            console.log("Couldn't insert");  
+        }else{
+            console.log('todo saved');   
+        }
+    })
 
 })
 
